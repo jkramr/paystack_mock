401b78c00bd0a93d3f55325b48513a05ea9df42b
jkramr <yuliya.kramarenko@gmail.com>
2017-04-18 12:46:43 +0300

    Refactoring.

--- src/main/java/eu/taxify/mock/paystack/response/Authorization.java
+++ src/main/java/eu/taxify/mock/paystack/response/Authorization.java
@@ -10,64 +10,43 @@ import lombok.NoArgsConstructor;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Authorization {
 
-  @JsonProperty("authorization_code")
-  private String authorizationCode;
-
-  private Integer bin;
-
-  private Integer last4;
-
-  @JsonProperty("exp_month")
-  private Integer expiryMonth;
-
-  @JsonProperty("exp_year")
-  private Integer expiryYear;
-
-  @JsonProperty("card_type")
-  private String cardType;
-
-  private String bank;
-
-  @JsonProperty("country_code")
-  private String countryCode;
-
-  private String brand;
-
-  private boolean reusable;
-
-  private String signature;
-
-
+  public static final String AUTHORISATION          = "      {\n" +
+                                                      "        \"authorization_code\": \"AUTH_xnf39jtr\",\n" +
+                                                      "        \"bin\": \"412345\",\n" +
+                                                      "        \"last4\": \"1381\",\n" +
+                                                      "        \"exp_month\": \"02\",\n" +
+                                                      "        \"exp_year\": \"2020\",\n" +
+                                                      "        \"channel\": \"card\",\n" +
+                                                      "        \"card_type\": \"visa\",\n" +
+                                                      "        \"bank\": \"TEST BANK\",\n" +
+                                                      "        \"country_code\": \"NG\",\n" +
+                                                      "        \"brand\": \"visa\",\n" +
+                                                      "        \"reusable\": true,\n" +
+                                                      "        \"signature\": \"SIG_y8PqDaob8DcMVqt9vHJj\"\n" +
+                                                      "      }\n";
   public static final String EXAMPLE_AUTHORIZATIONS =
           "    \"authorizations\": [\n" +
-          "      {\n" +
-          "        \"authorization_code\": \"AUTH_xnf39jtr\",\n" +
-          "        \"bin\": \"412345\",\n" +
-          "        \"last4\": \"1381\",\n" +
-          "        \"exp_month\": \"02\",\n" +
-          "        \"exp_year\": \"2020\",\n" +
-          "        \"channel\": \"card\",\n" +
-          "        \"card_type\": \"visa\",\n" +
-          "        \"bank\": \"TEST BANK\",\n" +
-          "        \"country_code\": \"NG\",\n" +
-          "        \"brand\": \"visa\",\n" +
-          "        \"reusable\": true,\n" +
-          "        \"signature\": \"SIG_y8PqDaob8DcMVqt9vHJj\"\n" +
-          "      }\n" +
+          AUTHORISATION +
           "    ],\n";
+  public static final String EXAMPLE_AUTHORIZATION =
+          "      \"authorization\":" +
+          AUTHORISATION + ",";
 
-  public static final String EXAMPLE_AUTHORIZATION  =
-          "      \"authorization\":{  \n" +
-          "         \"authorization_code\":\"AUTH_8dfhjjdt\",\n" +
-          "         \"card_type\":\"visa\",\n" +
-          "         \"last4\":\"1381\",\n" +
-          "         \"exp_month\":\"08\",\n" +
-          "         \"exp_year\":\"2018\",\n" +
-          "         \"bin\":\"412345\",\n" +
-          "         \"bank\":\"TEST BANK\",\n" +
-          "         \"channel\":\"card\",\n" +
-          "         \"reusable\":true,\n" +
-          "         \"country_code\":\"NG\",\n" +
-          "         \"signature\":\"SIG_7UkxQpDbSm3jQNKdo19uYaAFj8Teqx\"\n" +
-          "      },\n";
+
+  @JsonProperty("authorization_code")
+  private String authorizationCode;
+  private Integer bin;
+  private Integer last4;
+  @JsonProperty("exp_month")
+  private Integer expiryMonth;
+  @JsonProperty("exp_year")
+  private Integer expiryYear;
+  @JsonProperty("card_type")
+  private String cardType;
+  private String bank;
+  @JsonProperty("country_code")
+  private String countryCode;
+  private String brand;
+  private boolean reusable;
+  private String signature;
 }
